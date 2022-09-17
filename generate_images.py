3c0c8ad87a54dce08b9556a7d5ea4e9b4beb47eb
Thomas Kirschner <mail@thomaskirschner.de>
2022-09-17 21:13:20 +0200

    Do not ignore repos contributed to
    see exclusion here: https://github.com/tommy1005/github-stats/blob/master/github_stats.py#L337

--- generate_images.py
+++ generate_images.py
@@ -116,7 +116,7 @@ async def main() -> None:
         {x.strip() for x in exclude_langs.split(",")} if exclude_langs else None
     )
     # Convert a truthy value to a Boolean
-    raw_ignore_forked_repos = 'true'; # os.getenv("EXCLUDE_FORKED_REPOS")
+    raw_ignore_forked_repos = 'false'; # os.getenv("EXCLUDE_FORKED_REPOS")
     ignore_forked_repos = (
         not not raw_ignore_forked_repos
         and raw_ignore_forked_repos.strip().lower() != "false"
